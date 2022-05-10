27549d5863c7dee350e4592b7e10aa12bc7a598c
Oli Sturm <oliver@oliversturm.com>
2022-05-10 12:12:16 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { memo } from 'react';
+import styles from './App.module.css';
+import Calculator from './Calculator';
+
+const App = () => {
+  return (
+    <div className={styles.container}>
+      <h3>Calculators</h3>
+      <div className={styles.calcbox}>
+        <Calculator />
+        <Calculator />
+      </div>
+    </div>
+  );
+};
+
+export default memo(App);
---
+++ src/Calculator.js
@@ -0,0 +1,48 @@
+import { memo } from 'react';
+import BinOpButton from './BinOpButton';
+
+import styles from './Calculator.module.css';
+import { CalculatorContext, useCalculatorDefaultData } from './calculatorData';
+import CharButton from './CharButton';
+import Display from './Display';
+import EnterButton from './EnterButton';
+
+const Calculator = () => {
+  const data = useCalculatorDefaultData();
+
+  return (
+    <CalculatorContext.Provider value={data}>
+      <div className={styles.outer}>
+        <Display />
+        <div className={styles.keys}>
+          <CharButton char="7" />
+          <CharButton char="8" />
+          <CharButton char="9" />
+          <BinOpButton op="divide" />
+
+          <CharButton char="4" />
+          <CharButton char="5" />
+          <CharButton char="6" />
+          <BinOpButton op="multiply" />
+
+          <CharButton char="1" />
+          <CharButton char="2" />
+          <CharButton char="3" />
+          <BinOpButton op="minus" />
+
+          <div />
+          <CharButton char="0" />
+          <CharButton char="." once={true} />
+          <BinOpButton op="plus" />
+
+          <div />
+          <div />
+          <div />
+          <EnterButton />
+        </div>
+      </div>
+    </CalculatorContext.Provider>
+  );
+};
+
+export default memo(Calculator);
